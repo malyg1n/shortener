1f513d2c7ff50d5e05fb32d64d7f72376f4610a0
malyg1n <malygin.andrey85@gmail.com>
2022-01-22 20:16:16 +0500

    ref

--- api/rest/server.go
+++ api/rest/server.go
@@ -31,13 +31,15 @@ func RunServer(ctx context.Context) error {
 	}
 
 	router := chi.NewRouter()
+	router.Use(middleware.Compress)
+	router.Use(middleware.Decompress)
 	router.Get("/{linkId}", handler.GetLink)
 	router.Post("/", handler.SetLink)
 	router.Post("/api/shorten", handler.APISetLink)
 
 	srv := &http.Server{
 		Addr:    cfg.Addr,
-		Handler: middleware.Compress(middleware.Decompress(router)),
+		Handler: router,
 	}
 
 	go func() {
