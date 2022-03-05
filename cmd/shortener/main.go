f0757d8e3b9f0a74d8085e662d56a5b6389cfcd2
malyg1n <malygin.andrey85@gmail.com>
2022-03-05 21:50:40 +0500

    add profiles

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/malyg1n/shortener/api/rest"
 	"github.com/malyg1n/shortener/pkg/config"
 	v1 "github.com/malyg1n/shortener/services/linker/v1"
@@ -15,13 +14,11 @@ import (
 func main() {
 	ctx, ctxCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer ctxCancel()
-	log.Println("run main")
 
 	storage, err := pgsql.NewLinksStoragePG(ctx)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
-	log.Println("run storage")
 
 	defer storage.Close()
 
@@ -29,16 +26,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
-
-	log.Println("run service")
 	cfg := config.GetConfig()
-	log.Println(fmt.Sprintf("%v, %v, %v, %v, %v", cfg.Addr, cfg.BaseURL, cfg.DatabaseDSN, cfg.FileStoragePath, cfg.SecretKey))
 
 	server, err := rest.NewAPIServer(service, cfg.Addr)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
-	log.Println("server created")
 
 	server.Run(ctx)
 
